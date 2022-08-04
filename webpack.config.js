3739f13a8de007bcbab820d5e4645f4d6923c8a9
Shota Kubota <ta2@o2p.jp>
2022-08-04 19:46:54 +0900

    build: remove preset-env options

--- webpack.config.js
+++ webpack.config.js
@@ -38,16 +38,7 @@ module.exports = [
           use: {
             loader: "babel-loader",
             options: {
-              presets: [
-                [
-                  "@babel/preset-env",
-                  {
-                    modules: "cjs",
-                    spec: true,
-                  },
-                ],
-                "@babel/preset-react",
-              ],
+              presets: [["@babel/preset-env"], "@babel/preset-react"],
             },
           },
         },
@@ -80,16 +71,7 @@ module.exports = [
           use: {
             loader: "babel-loader",
             options: {
-              presets: [
-                [
-                  "@babel/preset-env",
-                  {
-                    modules: "cjs",
-                    spec: true,
-                  },
-                ],
-                "@babel/preset-react",
-              ],
+              presets: [["@babel/preset-env"], "@babel/preset-react"],
             },
           },
         },
