3987f18e71e5940826f136cdf38ba05e7c6926fa
Shota Kubota <ta2@o2p.jp>
2022-08-04 22:31:20 +0900

    tweak

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,9 +1,8 @@
 import "./foundation/side-effects";
 
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 
 import { App } from "./foundation/App";
 
-const root = document.getElementById("root");
-ReactDOM.render(<App />, root);
+render(<App />, document.getElementById("root"));
