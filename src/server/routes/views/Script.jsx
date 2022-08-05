25a31146455af837e7830fc849f390e9ef1ef13a
Shota Kubota <ta2@o2p.jp>
2022-08-05 19:52:56 +0900

    feat: 重要なJSは最優先にしたい

--- src/server/routes/views/Script.jsx
+++ src/server/routes/views/Script.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 export const Script = () => (
   <>
+    <link as="script" href="/framework.js" rel="preload" />
+    <link as="script" href="/vendor.js" rel="preload" />
+    <link as="script" href="/main.js" rel="preload" />
     <script defer src="/framework.js" />
     <script defer src="/vendor.js" />
     <script defer src="/main.js" />
